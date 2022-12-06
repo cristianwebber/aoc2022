7bc1a77a09bcd4a3769cace8efc38d3e907c9f47
Cristian Webber <cristian.webber@hotmail.com>
2022-12-06 08:56:02 -0300

    day 6 part 1

---
+++ day06/part1.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    for idx in range(len(s)):
+        no_repeated = s[idx:idx+4]
+        if len(set(no_repeated)) == 4:
+            return idx+4
+    return 0
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        ('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 7),
+        ('bvwbjplbgvbhsrlpgdmjqwftvncz', 5),
+        ('nppdvjthqldpwncqszvftbrmjlhg', 6),
+        ('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10),
+        ('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
