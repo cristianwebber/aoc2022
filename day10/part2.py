8d895d11ba9a5c2babafa95503b3930da2d77abe
Cristian Webber <cristian.webber@hotmail.com>
2022-12-10 17:16:33 -0300

    day 10 part 2

---
+++ day10/part2.py
@@ -0,0 +1,225 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> list[str]:
+    X = 1
+    cycle_X = []
+    draw = []
+
+    lines = s.splitlines()
+    for line in lines:
+        if line == 'noop':
+            cycle_X.append(X)
+        else:
+            cycle_X.append(X)
+            cycle_X.append(X)
+            X += int(line.split(' ')[1])
+
+    for i in range(240):
+        mid = cycle_X[i]
+        if i % 40 in (mid-1, mid, mid+1):
+            draw.append('#')
+        else:
+            draw.append('.')
+
+    final = [''.join(draw[40*i:40*(i+1)]) for i in range(6)]
+
+    for i in final:  # type: ignore
+        print(i)
+
+    print('\n\n')
+
+    return final
+
+
+INPUT_S = '''\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+'''
+
+EXPECTED = [
+    '##..##..##..##..##..##..##..##..##..##..',
+    '###...###...###...###...###...###...###.',
+    '####....####....####....####....####....',
+    '#####.....#####.....#####.....#####.....',
+    '######......######......######......####',
+    '#######.......#######.......#######.....',
+]
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
