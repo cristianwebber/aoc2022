c69eef228711df028674ad40889e0b623c679e2f
Cristian Webber <cristian.webber@hotmail.com>
2022-12-17 08:10:55 -0300

    day15 part1

---
+++ day15/part1.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+import re
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str, y: int = 2_000_000) -> int:
+    covered = set()
+    beacons = set()
+
+    lines = s.splitlines()
+    for line in lines:
+        sx, sy, bx, by = re.findall('=([0-9-]*)', line)
+        sensor = (int(sx), int(sy))
+        beacon = (int(bx), int(by))
+
+        if beacon[1] == y:
+            beacons.add(beacon)
+
+        distance = abs(sensor[0] - beacon[0]) + abs(sensor[1] - beacon[1])
+        distance_y = distance - abs(y - sensor[1])
+
+        for x in range(sensor[0] - distance_y, sensor[0] + distance_y + 1):
+            covered.add((x, y))
+
+    return len(covered - beacons)
+
+
+INPUT_S = '''\
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
+'''
+EXPECTED = 26
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s, y=10) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
